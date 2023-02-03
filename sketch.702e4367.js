08ad00355065028fd462f649d03ad8e7f8bad4b0
Vergel Linsangan <40718012+vergell@users.noreply.github.com>
2023-02-03 09:37:16 +0800

    Add files via upload

--- sketch.702e4367.js
+++ sketch.702e4367.js
@@ -29000,7 +29000,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56480" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60914" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
